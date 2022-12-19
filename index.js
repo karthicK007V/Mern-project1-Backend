0b44787510ba398cd5431ff20085c568a01c56ca
Karthick V <108870718+karthicK007V@users.noreply.github.com>
2022-12-19 18:16:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express=require("express");
+const dotenv=require("dotenv");
+const db=require("./db/connect");
+const cors=require("cors")
+const userRouter=require("./route/userRouter")
+const productRouter=require("./route/productroute")
+
+
+
+
+
+
+dotenv.config()
+const app=express();
+app.use(express.json());
+
+
+// connection
+db();
+app.use(cors())
+
+
+
+//Middleware
+app.use("/api",userRouter);
+app.use("/api",productRouter);
+
+
+
+
+
+const port=process.env.PORT||7000;
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`App is Running http://localhost:${port}`);
+});
